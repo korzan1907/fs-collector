49c066fdfbed7ec678938313ce11baed4e7a1ac5
Dave Weilert <daveweilert@MacBook-Pro.local>
2019-01-13 09:45:46 -0600

    Initial load

--- build_push.sh
+++ build_push.sh
@@ -1,3 +1,2 @@
-docker build -t ibmicpcoc/collector:latest -t ibmicpcoc/collector .
+docker build -t ibmicpcoc/collector:latest .
 docker push ibmicpcoc/collector:latest
-docker push ibmicpcoc/collector
